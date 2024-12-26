2cee80fbee1e5633b7224d7db36dbfbd314dd618
Syed Ramim <parthib004@gmail.com>
2024-12-26 17:37:18 -0500

    added styling to main view

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,54 @@
-import { Text, View } from "react-native";
+import { View, Text, Button, StyleSheet } from 'react-native'
+import React, { useState } from 'react'
+import { quotes } from './quote'
 
-export default function Index() {
+const index = () => {
+  const generateNewQuote = () => {
+    return quotes[Math.floor(Math.random() * quotes.length)]
+  }
+  const [quote, setQuote] = useState(generateNewQuote())
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={styles.container}>
+      <Text style={styles.text}>{quote.text}</Text>
+      <Text style={styles.text}>{quote.author}</Text>
+
+      <Button
+        onPress={() => setQuote(generateNewQuote())}
+        title='New Quote'
+        
+      />
     </View>
-  );
+  )
 }
+
+const styles = StyleSheet.create({
+  container:{
+    flex: 1,
+    padding: 40,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  text:{
+    fontSize: 24,
+    borderColor: 'black',
+    borderWidth: 4,
+    borderRadius: 10,
+    fontWeight: 'bold',
+    width: 400
+    
+  },
+  author:{
+    fontSize: 16,
+    borderColor: 'black',
+    borderWidth: 2,
+    borderRadius: 5,
+    width: 200
+  },
+  button: {
+    width: 200,
+    height: 100
+  }
+})
+
+export default index
---
+++ app/quote.jsx
@@ -0,0 +1,42 @@
+export const quotes = [
+    {
+      text: "The only way to do great work is to love what you do.",
+      author: "Steve Jobs"
+    },
+    {
+      text: "Success is not final, failure is not fatal: it is the courage to continue that counts.",
+      author: "Winston Churchill"
+    },
+    {
+      text: "Everything you've ever wanted is on the other side of fear.",
+      author: "George Addair"
+    },
+    {
+      text: "The future belongs to those who believe in the beauty of their dreams.",
+      author: "Eleanor Roosevelt"
+    },
+    {
+      text: "I have not failed. I've just found 10,000 ways that won't work.",
+      author: "Thomas Edison"
+    },
+    {
+      text: "The way to get started is to quit talking and begin doing.",
+      author: "Walt Disney"
+    },
+    {
+      text: "Don't watch the clock; do what it does. Keep going.",
+      author: "Sam Levenson"
+    },
+    {
+      text: "The only limit to our realization of tomorrow will be our doubts of today.",
+      author: "Franklin D. Roosevelt"
+    },
+    {
+      text: "It does not matter how slowly you go as long as you do not stop.",
+      author: "Confucius"
+    },
+    {
+      text: "Everything is hard before it is easy.",
+      author: "Johann Wolfgang von Goethe"
+    }
+  ];
